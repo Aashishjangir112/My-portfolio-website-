7de0cd01c23dd0d72987a809c2eb43179c219e39
Aashish jangir <aashishjangir1123@gmail.com>
2024-02-28 12:38:39 +0530

    first commit

---
+++ aashish jangir main/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Shivam Kumar</span> Working in Amazon as GO AI Associate, Enthusiastic Technology with a demonstrated history of learning in the information technology and services industry. Skilled in Devops, Hybrid Multicloud, Web development with multiple tools and technologies like GKE,VPC, Google Cloud Platform, Docker ,Kubernetes, Jenkins, Github, SQL, IOT & Exploring API's. <br><br> Strong information technology professional with Master of Computer Application - MCA focused in Computer Science from Jagran Institute of Management, Kanpur 
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
